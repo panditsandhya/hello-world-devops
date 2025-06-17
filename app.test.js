1ad992703d7cfd6822903a5f39b46c4c92a12919
Sandhya <starsandhya369@gmail.com>
2025-06-17 12:41:04 +0530

    reinstall husky

--- app.test.js
+++ app.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
-const app = require('./app');
+const app = require('./app'); 
 
 describe('GET /', () => {
   it('should respond with Hello, World!', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello');
+    expect(res.text).toBe('Hello, World!');
   });
-});
+
